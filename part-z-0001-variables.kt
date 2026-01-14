9214988b2a0c37e03a370e4a58b3bf8be2ce3fe2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-14 20:08:34 +0300

    Create part-z-0001-variables.kt

---
+++ part-z-0001-variables.kt
@@ -0,0 +1,19 @@
+fun main() {
+    
+    var budgetYear = 2025
+    
+    var salesTotal = 22_500_000
+    
+    salesTotal = 24_000_000
+    
+    var companyName = "Company A"
+    
+    println(budgetYear)
+    println(salesTotal)
+    println(companyName)
+   
+}
+
+// 2025
+// 24000000
+// Company A
