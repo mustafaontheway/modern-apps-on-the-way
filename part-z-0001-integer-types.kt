9be5644957a2a55a0580eabf7119f3c8a1aaf4b5
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-14 20:18:50 +0300

    Create part-z-0001-integer-types.kt

---
+++ part-z-0001-integer-types.kt
@@ -0,0 +1,19 @@
+fun main() {
+    
+	var age = 17
+    
+    var myNegValue: Byte = -12
+    
+    var herAge: UByte = 24u
+    
+    var sales: UInt = 210000u
+    
+    var year = 2026 
+    
+    var x: Long = 21_900_650L
+    
+    var y: Short = -780
+    
+    var loss: Int = -50000
+   
+}
