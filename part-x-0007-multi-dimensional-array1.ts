37752558dfbf8996b4cd22c91e05f985aa068611
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-16 00:13:06 +0300

    Create part-x-0007-multi-dimensional-array1.ts

---
+++ part-x-0007-multi-dimensional-array1.ts
@@ -0,0 +1,10 @@
+let quarterlyEmployeeNums: number[][] = [
+    [100, 120, 141],
+    [96, 75, 102],
+    [105, 108, 111],
+    [97, 85, 106]
+]
+
+quarterlyEmployeeNums[1][2] = 110
+
+console.log(`May employee number: ${quarterlyEmployeeNums[1][1]}`) // May employee number: 75
