f44fea23b737866363b258655340918e25b1683a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-16 00:11:11 +0300

    Create part-x-0006-array1.ts

---
+++ part-x-0006-array1.ts
@@ -0,0 +1,14 @@
+let ages: number[] = [17, 96, 44, 55, 74, 80, 3]
+
+ages.push(40)
+ages.push(90)
+
+console.log(ages)
+console.log(ages[2])
+
+// [
+//   17, 96, 44, 55, 74,
+//   80,  3, 40, 90
+// ]
+
+// 44
