1e1d32e2c17aff7decd07344587edb5027ac6140
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-15 09:46:48 +0300

    Create part-z-0008-constant.kt

---
+++ part-z-0008-constant.kt
@@ -0,0 +1,12 @@
+fun main() {
+    
+    var x = 22 
+    
+    x = 21
+    
+    println(x) // 21
+    
+    val y = 21.12 // constant!
+    
+    //y = 65 // Error => 'val' cannot be reassigned.
+}
