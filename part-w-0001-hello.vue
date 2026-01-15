3041f09ec5239922d60723e665d51c3e382b5365
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-15 23:39:54 +0300

    Create part-w-0001-hello.vue

---
+++ part-w-0001-hello.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+  const year = 2026
+
+  function handleClick() {
+
+    console.log("You clicked!")
+  }
+
+</script>
+
+<template>
+
+  <h1>Hello World!</h1>
+
+  <p>In {{ year }}, I'll do my best!</p>
+
+  <button @click="handleClick">Click It</button>
+  
+</template>
