2c5bf8a7322e2837a7ae0c864f77f8b457eb2c2e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-16 00:00:18 +0300

    Create part-w-0003-reactive1.vue

---
+++ part-w-0003-reactive1.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+  import {reactive} from "vue"
+
+  const counterObj = reactive({
+    
+    title: "Counter App",
+    counter: 0
+  })
+
+  function counterUp() {
+
+    counterObj.counter++
+  }
+
+</script>
+
+<template>
+
+  <h2>{{ counterObj.title }}</h2>
+
+  <p>Counter: {{ counterObj.counter }}</p>
+
+  <button @click="counterUp">Counter ++</button>
+
+</template>
+
+
