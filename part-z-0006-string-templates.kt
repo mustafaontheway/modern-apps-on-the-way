456e196c9501bd1f39cca22ea981a98a792e4a22
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-15 09:44:37 +0300

    Create part-z-0006-string-templates.kt

---
+++ part-z-0006-string-templates.kt
@@ -0,0 +1,18 @@
+fun main() {
+    
+	var name = "Aybüke"
+    
+    var place = "Greenland"
+    
+    println("$name is from $place")
+    
+    var x = 22 
+    
+    var y = 54 
+    
+    println("$x + $y = ${x + y}")
+   
+}
+
+// Aybüke is from Greenland
+// 22 + 54 = 76
