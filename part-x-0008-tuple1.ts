867e1390b64d7dcae950acd6ea89bc92982bdf39
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-16 00:13:49 +0300

    Create part-x-0008-tuple1.ts

---
+++ part-x-0008-tuple1.ts
@@ -0,0 +1,16 @@
+let empAyhan: [string, string, number, boolean] = ["Ayhan Bilir", "ab0041", 55_000, false]
+
+console.log(empAyhan[0])
+console.log(empAyhan[1])
+console.log(empAyhan[2])
+console.log(empAyhan[3])
+
+empAyhan[3] = true
+
+console.log(`Is Ayhan Bilir married: ${empAyhan[3]}`)
+
+// Ayhan Bilir
+// ab0041
+// 55000
+// false
+// Is Ayhan Bilir married: true
