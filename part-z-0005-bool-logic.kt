e8e24e22d9598a9b82eed7162bcbc9a917237e62
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-15 09:43:02 +0300

    Create part-z-0005-bool-logic.kt

---
+++ part-z-0005-bool-logic.kt
@@ -0,0 +1,22 @@
+fun main() {
+    
+	var isReady = true 
+    
+    var completed: Boolean = false 
+    
+    var isTrue = 5 > 2
+    
+    var l1 = 5 != 5 
+    
+    var l2 = (5 > 1) && (2 != 3)
+    
+    println(l2)
+    
+    var l3 = (5 > 1) || (2 == 3)
+    
+    println(l3)
+   
+}
+
+// true
+// true
