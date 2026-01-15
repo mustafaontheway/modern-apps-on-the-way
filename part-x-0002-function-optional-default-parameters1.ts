da95ac7671bfc14c5b53caeb3fa3d0701de226ea
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-16 00:07:41 +0300

    Create part-x-0002-function-optional-default-parameters1.ts

---
+++ part-x-0002-function-optional-default-parameters1.ts
@@ -0,0 +1,19 @@
+function sum(x: number, y: number, z?: number) : number {
+
+    if (z === undefined) {
+
+        return x + y
+      
+    } else {
+
+        return x + y + z
+    }
+}
+
+let result1 = sum(-500, 2147.12)
+
+console.log(result1) // 1647.12
+
+let result2 = sum(-500, 2147.12, 9999)
+
+console.log(result2) // 11646.119999999999
